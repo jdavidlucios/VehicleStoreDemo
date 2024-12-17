8d09518749e3ecbc8f4b7b24a7123cd00927a984
jdavidlucios <juandalucio@gmail.com>
2024-12-16 23:42:02 -0300

    Registration and List views + project structure order

--- proyecto_vehiculos_django/vehiculo/urls.py
+++ proyecto_vehiculos_django/vehiculo/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-from .views import agregar_vehiculo
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('vehiculo_add', agregar_vehiculo, name='vehiculo/add'),
-    path('vehiculo_list', agregar_vehiculo, name='vehiculo/list'),
+    path('vehiculo/add/', views.agregar_vehiculo, name='add'),
+    path('vehiculo/listar', views.listar_vehiculos, name='listar'),
 ]
